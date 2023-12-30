16e8b1dd8c73cd5fe1262de5e6d882e9071c8a4e
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-12-30 15:54:22 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,28 +11,31 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     basic.showNumber(iDisplay)
 })
 function fDisplay () {
-    _4digit.show(sendeBuffer.getUint8(iDisplay))
+    if (i2c.between(iDisplay, 0, 3)) {
+        _4digit.show(sendeBuffer.getUint8(iDisplay))
+    }
 }
 let iServo = 0
 let iMotor = 0
-let oBuffer: i2c.i2cclass = null
 let iDisplay = 0
 let sendeBuffer: i2c.i2cclass = null
 let _4digit: grove.TM1637 = null
 _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+qwiicjoystick.beimStart(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20))
+let aJoy = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.B_0_255)
 radio.setGroup(240)
 sendeBuffer = i2c.create(4)
 iDisplay = 0
 loops.everyInterval(400, function () {
     basic.setLedColor(7)
-    oBuffer = i2c.i2cReadBuffer(i2c.i2c_eADDR(i2c.eADDR.Joystick_x52), 3)
-    iMotor = oBuffer.getUint8(0)
+    aJoy = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.B_0_255)
+    iMotor = aJoy[0]
     i2c.comment("0 Motor 0..128..255")
     if (i2c.between(iMotor, 120, 136)) {
         iMotor = 128
     }
-    iServo = oBuffer.getUint8(1)
-    iServo = Math.round(Math.map(iServo, 0, 255, 45, 135))
+    iServo = aJoy[1]
+    iServo = Math.round(Math.map(iServo, 0, 255, 135, 45))
     i2c.comment("1 Servo 0..128..255 -> 45..90..135")
     if (i2c.between(iServo, 86, 94)) {
         iServo = 90
