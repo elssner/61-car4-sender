4966461d4f89186760bb68aa101320533c23b59b
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-12-30 19:33:32 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,14 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     }
     basic.showNumber(iDisplay)
 })
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    iFahrstrecke = 30
+    basic.setLedColor(0x00ff00)
+    sendeBuffer.setUint8(0, 155)
+    sendeBuffer.setUint8(1, 90)
+    sendeBuffer.setUint8(2, iFahrstrecke)
+    radio.sendNumber(sendeBuffer.getNumber(NumberFormat.UInt32LE, 0))
+})
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     if (iDisplay < 3) {
         iDisplay += 1
@@ -15,8 +23,12 @@ function fDisplay () {
         _4digit.show(sendeBuffer.getUint8(iDisplay))
     }
 }
+input.onButtonEvent(Button.AB, input.buttonEventValue(ButtonEvent.Hold), function () {
+    iFahrstrecke = 0
+})
 let iServo = 0
 let iMotor = 0
+let iFahrstrecke = 0
 let iDisplay = 0
 let sendeBuffer: i2c.i2cclass = null
 let _4digit: grove.TM1637 = null
@@ -26,23 +38,29 @@ let aJoy = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoysti
 radio.setGroup(240)
 sendeBuffer = i2c.create(4)
 iDisplay = -1
+iFahrstrecke = 0
 loops.everyInterval(400, function () {
-    basic.setLedColor(7)
-    aJoy = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.B_0_255)
-    iMotor = aJoy[0]
-    i2c.comment("0 Motor 0..128..255")
-    if (i2c.between(iMotor, 120, 136)) {
-        iMotor = 128
+    if (iFahrstrecke == 0) {
+        basic.setLedColor(7)
+        aJoy = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.B_0_255)
+        iMotor = aJoy[0]
+        i2c.comment("0 Motor 0..128..255")
+        if (i2c.between(iMotor, 120, 136)) {
+            iMotor = 128
+        }
+        iServo = aJoy[1]
+        iServo = Math.round(Math.map(iServo, 0, 255, 135, 45))
+        i2c.comment("1 Servo 0..128..255 -> 45..90..135")
+        if (i2c.between(iServo, 86, 94)) {
+            iServo = 90
+        }
+        sendeBuffer.setUint8(0, iMotor)
+        sendeBuffer.setUint8(1, iServo)
+        sendeBuffer.setUint8(2, 0)
+        radio.sendNumber(sendeBuffer.getNumber(NumberFormat.UInt32LE, 0))
+        basic.turnRgbLedOff()
+    } else {
+    	
     }
-    iServo = aJoy[1]
-    iServo = Math.round(Math.map(iServo, 0, 255, 135, 45))
-    i2c.comment("1 Servo 0..128..255 -> 45..90..135")
-    if (i2c.between(iServo, 86, 94)) {
-        iServo = 90
-    }
-    sendeBuffer.setUint8(0, iMotor)
-    sendeBuffer.setUint8(1, iServo)
-    radio.sendNumber(sendeBuffer.getNumber(NumberFormat.UInt32LE, 0))
     fDisplay()
-    basic.turnRgbLedOff()
 })
